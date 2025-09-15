fb245cd73bd6f3f9dd460037c3c9010799d1e936
DavidNabil210 <davidnabil633@gmail.com>
2025-09-15 20:43:44 +0300

    feat(navbar): add dark mode toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,46 @@
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import { LogOut, Moon, Settings, User } from 'lucide-react'
+import { LogOut, Moon, Settings, Sun, User } from 'lucide-react'
 import Link from "next/link"
 import React from 'react'
+import { Button } from "./ui/button"
+import { useTheme } from "next-themes"
 
 
 export const Navbar = () => {
+  const { setTheme } = useTheme();
+
   return (
     <nav className='p-4 flex items-center justify-between'>
       {/* left */}
       collapseButton
       {/* right */}
-      <div className="flex items-center">
+      <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
-        <Moon />
+        {/* Dark system */}
+        <DropdownMenu >
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {/* Dropdown Menu */}
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Avatar className="w-10 h-10">
@@ -40,7 +67,7 @@ export const Navbar = () => {
             </DropdownMenuItem>
             <DropdownMenuItem>Team</DropdownMenuItem>
             <DropdownMenuSeparator />
-            <DropdownMenuItem variant="destructive">LogOut <LogOut/></DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">LogOut <LogOut /></DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
